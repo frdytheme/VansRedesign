1ece760b7b4d4c12c31dcc1ea29b47d916c63dcb
FRDY <rbfridayy@gmail.com>
2023-09-18 16:08:22 +0900

    배포용 url 수정

--- src/assets/api/authApi.js
+++ src/assets/api/authApi.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // 반응형...??
 
 const authApi = axios.create({
-  // baseURL: "https://vans-redesign-frdytheme.koyeb.app/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://vans-redesign-frdytheme.koyeb.app/api",
+  // baseURL: "http://localhost:8000/api",
   withCredentials: true,
 });
 
